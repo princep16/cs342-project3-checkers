da67b9819e10d39fbacf688563dfd0e6ca79b6f4
Prince Patel <patelprince1609@gmail.com>
2026-09-03 13:07:12 -0500

    Replace server placeholder test with message validation tests

--- hw5.startercode/HW5Server/src/test/java/MyTest.java
+++ hw5.startercode/HW5Server/src/test/java/MyTest.java
@@ -1,17 +1,41 @@
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.ObjectStreamClass;
+
 import org.junit.jupiter.api.Test;
 
-import org.junit.jupiter.api.DisplayName;
+class ServerMessageTest {
 
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
+    @Test
+    void initializesServerMessageCollections() {
+        Message message = new Message();
 
-class MyTest {
+        assertNotNull(message.activeUsers);
+        assertNotNull(message.validMoves);
+        assertNotNull(message.onlineFriends);
+    }
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
+    @Test
+    void supportsCoreServerMessageTypes() {
+        Message message = new Message();
 
+        message.type = Message.MessageType.GAME_START;
+        message.sender = "PlayerOne";
+        message.recipient = "PlayerTwo";
+        message.playerColor = 1;
+
+        assertEquals(Message.MessageType.GAME_START, message.type);
+        assertEquals("PlayerOne", message.sender);
+        assertEquals("PlayerTwo", message.recipient);
+        assertEquals(1, message.playerColor);
+    }
+
+    @Test
+    void usesExpectedSerializationVersion() {
+        long serialVersion = ObjectStreamClass
+                .lookup(Message.class)
+                .getSerialVersionUID();
+
+        assertEquals(42L, serialVersion);
+    }
 }
